e8062840939189bda5103612134e2a9675113854
Yeikol <yeigamer1@gmail.com>
2024-11-16 22:15:00 -0400

    Tarea 1

---
+++ char_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	char str_a[20];
+	str_a[0] = 'Y';
+	str_a[1] = 'e';
+	str_a[2] = 'i';
+	str_a[3] = 'k';
+	str_a[4] = 'o';
+	str_a[5] = 'l';
+	str_a[6] = '!';
+	str_a[7] = '\n';
+	str_a[8] = 0;
+	printf(str_a);
+	return 0;
+}
